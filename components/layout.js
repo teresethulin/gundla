b89b4c96b2f68e2e0ff2896343f0e97785b00857
teresethulin <terese.thulin@gmail.com>
2020-09-08 11:46:58 +0200

    Add layout component

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const Layout = () => (
+  <main>
+    <Head>
+      <title>Gundla gårdscafé</title>
+    </Head>
+    <h1>Gundla gårdscafé</h1>
+  </main>
+);
+
+export default Layout;
