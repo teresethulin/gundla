c8c2281c03eedd7920f79c4f57c24f09c11bdd5f
teresethulin <terese.thulin@gmail.com>
2020-09-10 15:32:17 +0200

    Add links to header

--- components/header.js
+++ components/header.js
@@ -1,11 +1,47 @@
 import Link from "next/link";
 
 const Header = () => (
-  <h2>
-    <Link href="/">
-      <a>Gundla gårdscafé</a>
-    </Link>
-  </h2>
+  <div>
+    <h2>
+      <Link href="/">
+        <a>Gundla gårdscafé</a>
+      </Link>
+    </h2>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/about" as="/about">
+            <a>Om oss</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/booking" as="/boka">
+            <a>Bokningsförfrågan</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/catering" as="/catering">
+            <a>Catering</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/event" as="/event">
+            <a>Event</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/menu" as="/meny">
+            <a>Meny</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/renting" as="/fest">
+            <a>Fest och bröllop</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Header;
