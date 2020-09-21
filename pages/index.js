3e0ceee947efb05992edb58d09f99504a0812015
teresethulin <terese.thulin@gmail.com>
2020-09-21 10:04:58 +0200

    fetch instagram pictures index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import Layout from "../components/layout";
 import { getData } from "../lib/api";
 
+async function getInstagramPictures(profileName) {
+  const baseUrl = "https://www.instagram.com";
+  const profileUrl = `${baseUrl}/${profileName}`;
+  const jsonDataUrl = `${profileUrl}/?__a=1`;
+
+  const response = await fetch(jsonDataUrl);
+  const jsonData = await response.json();
+  const pictures = jsonData.graphql.user.edge_owner_to_timeline_media.edges;
+
+  if (response.ok) {
+    return pictures;
+  } else {
+    throw new Error(pictures);
+  }
+}
+
 export default function Home({ info }) {
+  getInstagramPictures("gundlagardscafe")
+    .then((pictures) => console.log(pictures))
+    .catch((error) => console.error("Error:", error));
+
   return (
     <Layout pageTitle="">
       <h1>Välkommen</h1>
