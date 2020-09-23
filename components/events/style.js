da3f0fecf6492e73bb74b5a6c3dcefdd539adb86
Jakob Clausen <jakob.fridmar@gmail.com>
2020-09-23 10:05:34 +0200

    Added media-q

--- components/events/style.js
+++ components/events/style.js
@@ -5,10 +5,13 @@ import { animated } from "react-spring";
 // Booking.js
 export const ContainerBooking = styled.div`
   width: 100%;
-  background-color: var(--main-bg-yellow);
+  background-color: var(--main-bg-brown);
   padding: 32px 22px;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.16), 2px 2px 0px rgba(0, 0, 0, 0.16);
-  margin: 30px 0px;
+  margin: 0px;
+  @media (min-width: 768px) {
+    padding: 32px 82px;
+  }
 `;
 export const FlexContainer = styled.div`
   width: 100%;
@@ -36,7 +39,7 @@ export const Input = styled(Field)`
   height: 40px;
   font-size: 16px;
   padding-left: 5px;
-  background-color: #e6e6e6;
+  background-color: #ffffff;
   margin-top: 5px;
   border: 0;
   position: relative;
@@ -56,9 +59,12 @@ export const ErrorMessage = styled.p`
 export const ContainerInfo = styled.div`
   width: 100%;
   padding: 32px 22px;
-  margin: 12px 0px;
-  background-color: var(--main-bg-yellow);
+  margin: 0px;
+  background-color: var(--main-bg-brown);
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25), 2px 2px 0px rgba(0, 0, 0, 0.16);
+  @media (min-width: 768px) {
+    padding: 70px 10vw;
+  }
 `;
 export const TitleInfo = styled.h3`
   font-style: normal;
@@ -73,6 +79,21 @@ export const DescInfo = styled.p`
 export const Image = styled.img`
   width: 100%;
 `;
+export const Bar = styled.div`
+  width: 100%;
+  height: 16px;
+  background-color: var(--main-bg-black);
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25), 2px 2px 0px rgba(0, 0, 0, 0.16);
+`;
+
+export const DesktopImage = styled.img`
+  display: none;
+  width: 100%;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25), 2px 2px 0px rgba(0, 0, 0, 0.16);
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
 
 // Event.js
 
@@ -80,7 +101,7 @@ export const CardEvent = styled.div`
   width: 100%;
   padding: 32px 22px;
   margin: 12px 0px;
-  background-color: var(--main-bg-yellow);
+  background-color: var(--main-bg-brown);
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25), 2px 2px 0px rgba(0, 0, 0, 0.16);
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -122,4 +143,7 @@ export const ContainerNotis = styled(animated.div)`
   padding: 12px 22px;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.16), 2px 2px 0px rgba(0, 0, 0, 0.16);
   margin: 20px 0px;
+  @media (min-width: 768px) {
+    padding: 12px 82px;
+  }
 `;
--- components/post/GreenPost.js
+++ components/post/GreenPost.js
@@ -11,6 +11,14 @@ const Container = styled.div`
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25), 2px 2px 0px rgba(0, 0, 0, 0.16);
   color: var(--main-text-black);
 `;
+
+const DesktopDiv = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 30% 70%;
+  }
+`;
+
 const Bar = styled.div`
   width: 100%;
   height: 16px;
@@ -22,8 +30,10 @@ export default function GreenPost({ post, children, email }) {
     <>
       <Bar />
       <Container>
-        {isTitle(post.title)}
-        {ReactHtmlParser(post.text)}
+        <DesktopDiv>
+          {isTitle(post.title)}
+          <div>{ReactHtmlParser(post.text)}</div>
+        </DesktopDiv>
         {children}
         {isEmail(email)}
       </Container>
