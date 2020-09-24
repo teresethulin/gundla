1a6ce841669572ccfec68d4077d4d8597863b9de
teresethulin <terese.thulin@gmail.com>
2020-09-24 11:41:03 +0200

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
 import { getData, getInstagramPictures } from "../lib/api";
+import { useEffect, useState } from "react";
 import InstagramGrid from "../components/index/InstagramGrid";
 import CardWelcome from "../components/index/CardWelcome";
 import CardFindUs from "../components/index/CardFindUs";
 import ImageLinks from "../components/ImageLinks";
 import OpeningHours from "../components/index/OpeningHours";
 
-export default function Home({ pictures, post, imageLinks, hours }) {
+export default function Home({ post, imageLinks, hours }) {
+  // const [pictures, setPictures] = useState(null);
+  // useEffect(async () => {
+  //   setPictures(await getInstagramPictures("gundlagardscafe"));
+  // });
+
   return (
     <div>
+      {console.log(pictures)}
       <CardWelcome post={post}></CardWelcome>
       <ImageLinks imageLinks={imageLinks}></ImageLinks>
       <CardFindUs post={post}></CardFindUs>
       <OpeningHours hours={hours}></OpeningHours>
-      <InstagramGrid pictures={pictures}></InstagramGrid>
+      {/* <InstagramGrid pictures={pictures}></InstagramGrid> */}
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const pictures = await getInstagramPictures("gundlagardscafe");
   const post = await getData("/start");
   const imageLinks = await getData("/image-links");
   const { hours } = await getData("/opening-hours");
   const info = await getData("/info");
 
   return {
-    props: { pictures, post, imageLinks, hours, info },
+    props: { post, imageLinks, hours, info },
   };
 }
